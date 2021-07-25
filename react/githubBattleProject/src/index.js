09f733ef29a6ef0782a54d3fdb453d9a45f94068
Toan <tpkhuong@gmail.com>
2021-07-25 06:47:31 -0400

    added Popular navbar component to entry file

--- react/githubBattleProject/src/index.js
+++ react/githubBattleProject/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../public/styles.css";
+import Popular from "./components/PopularNavbar/PopularNavbar";
 
 class App extends React.Component {
   render() {
-    return <div>Hello World! Yay</div>;
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
   }
 }
 
