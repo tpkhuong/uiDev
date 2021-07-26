be9d36726b78851e111f7b6ed5fbfc804b5f0bd8
Toan <tpkhuong@gmail.com>
2021-07-26 06:49:15 -0400

    added clicking feature to navbar using setState

--- react/githubBattleProject/src/components/PopularNavbar/PopularNavbar.js
+++ react/githubBattleProject/src/components/PopularNavbar/PopularNavbar.js
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({ selectedLanguage });
+  }
+
+  //since we are not passing updateLanguage to another component we dont need this.updateLanguage = this.updateLanguage.bind(this).
+  //we dont need to bind updateLanguage to the current context using the this keyword.
+
   render() {
     //   variable goes here
     var languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
@@ -9,12 +25,21 @@ class Popular extends React.Component {
       <ul className="flex-center">
         {languages.map((language, index) => (
           <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
+            <button
+              className="btn-clear nav-link"
+              style={
+                language === this.state.selectedLanguage
+                  ? { color: "rgb(187,46,31)" }
+                  : null
+              }
+              onClick={() => this.updateLanguage(language)}
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
     );
   }
 }
-
 export default Popular;
