2c06566ea08e3d6f57503211565020113375fe03
Toan <tpkhuong@gmail.com>
2021-07-27 06:33:04 -0400

    completed proptype exercise

--- react/exercise.js
+++ react/exercise.js
@@ -265,3 +265,64 @@ class Count extends React.Component {
 ReactDOM.render(<Count />, document.getElementById("root"));
 
 /* setState exercise 2 */
+
+/* Props.type exercise */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+class Badge extends React.Component {
+  render() {
+    const { authed, style, name, handle, img, addFriend } = this.props;
+
+    if (authed !== true) {
+      return <p>You need to log in.</p>;
+    }
+
+    return (
+      <div style={style}>
+        <img
+          alt="avatar"
+          style={{ width: 200, borderRadius: "50%" }}
+          src={img}
+        />
+        <h1 style={{ margin: 5 }}>{name}</h1>
+        <h3 style={{ margin: 5 }}>@{handle}</h3>
+        <button onClick={addFriend}>Add Friend</button>
+      </div>
+    );
+  }
+}
+
+Badge.propTypes = {
+  name: PropTypes.string.isRequired,
+  handle: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  authed: PropTypes.bool,
+  addFriend: PropTypes.func.isRequired,
+  style: PropTypes.object,
+};
+
+ReactDOM.render(
+  <Badge
+    name="Tyler McGinnis"
+    handle="tylermcginnis"
+    img="https://avatars0.githubusercontent.com/u/2933430?v=3&s=460"
+    authed={true}
+    style={{
+      width: 300,
+      margin: "0 auto",
+      border: "3px solid #000",
+      padding: 10,
+      borderRadius: 3,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    }}
+    addFriend={() => alert("Added!")}
+  />,
+  document.getElementById("root")
+);
+
+/* Props.type exercise */
