d359a1315c91df5ea94fc9c18d74ea6a22e6430e
Toan <tpkhuong@gmail.com>
2021-08-02 06:47:55 -0400

    added battle component to index.js

--- react/githubBattleProject/src/index.js
+++ react/githubBattleProject/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../public/styles.css";
 import Popular from "./components/PopularNavbar/PopularNavbar";
+import Battle from "./components/Battle/Battle";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Popular />
+        {/* <Popular /> */}
+        <Battle />
       </div>
     );
   }
