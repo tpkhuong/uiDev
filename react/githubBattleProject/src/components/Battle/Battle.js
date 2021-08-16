16da031059e0739e58bf4b3f1269cdf3012b4e12
Toan <tpkhuong@gmail.com>
2021-08-16 06:53:10 -0400

    dark mode toggle jsx

--- react/githubBattleProject/src/components/Battle/Battle.js
+++ react/githubBattleProject/src/components/Battle/Battle.js
@@ -7,30 +7,44 @@ import {
 } from "react-icons/fa";
 import PropTypes from "prop-types";
 import Results from "../Results/Results";
+import { ThemeConsumer } from "../../../context/theme";
 
 function Instructions() {
   return (
-    <div className="instructions-container">
-      <h1 className="center-text header-lg">Instructions</h1>
-      <ol className="container-sm grid center-text battle-instructions">
-        <li>
-          <h2 className="header-sm">Enter two Github users</h2>
-          <FaUserFriends
-            className="bg-light"
-            color="rgb(255,191,116)"
-            size={140}
-          />
-        </li>
-        <li>
-          <h2 className="header-sm">Battle</h2>
-          <FaFighterJet className="bg-light" color="#727272" size={140} />
-        </li>
-        <li>
-          <h2 className="header-sm">See the winners</h2>
-          <FaTrophy className="bg-light" color="rgb(255,215,0)" size={140} />
-        </li>
-      </ol>
-    </div>
+    <ThemeConsumer>
+      {/* accessing our Theme: passing a render prop our children put jsx inside of what we want our arrow func to returning */}
+      {({ theme, toggleTheme }) => (
+        <div className="instructions-container">
+          <h1 className="center-text header-lg">Instructions</h1>
+          <ol className="container-sm grid center-text battle-instructions">
+            <li>
+              <h2 className="header-sm">Enter two Github users</h2>
+              <FaUserFriends
+                className={`bg-${theme}`}
+                color="rgb(255,191,116)"
+                size={140}
+              />
+            </li>
+            <li>
+              <h2 className="header-sm">Battle</h2>
+              <FaFighterJet
+                className={`bg-${theme}`}
+                color="#727272"
+                size={140}
+              />
+            </li>
+            <li>
+              <h2 className="header-sm">See the winners</h2>
+              <FaTrophy
+                className={`bg-${theme}`}
+                color="rgb(255,215,0)"
+                size={140}
+              />
+            </li>
+          </ol>
+        </div>
+      )}
+    </ThemeConsumer>
   );
 }
 
@@ -71,39 +85,43 @@ class PlayerInput extends React.Component {
       /* we will render <PlayerIput> inside of <Battle> component pass a onSubmit prop. whenever <form> is submitted the prop(we will pass a function as prop) 
       onSubmit prop/func inside the <PlayerInput> component declaration which is inside the handleSubmit method will be invoked passing username
       */
-      <form className="column player" onSubmit={this.handleSubmit}>
-        <label htmlFor="username" className="player-label">
-          {this.props.label}
-        </label>
-        <div className="row player-inputs">
-          {/* Player Input video: controlled components. the value of the input will be whatever the value is assigned to this.state.username
+      <ThemeConsumer>
+        {({ theme, toggleTheme }) => (
+          <form className="column player" onSubmit={this.handleSubmit}>
+            <label htmlFor="username" className="player-label">
+              {this.props.label}
+            </label>
+            <div className="row player-inputs">
+              {/* Player Input video: controlled components. the value of the input will be whatever the value is assigned to this.state.username
           in the this.setState of the component in the constructor of the component. we are updating the local state using handleChange will call
           this.setState({username: event.target.value}) which will reRender. event.target.value will be whatever is typed into the input.
           */}
-          <input
-            type="text"
-            id="username"
-            className="input-light"
-            placeholder="github username"
-            autoComplete="off"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          {/* in the button element the disabled based on our this.state.username. when this.state.username has no value diabled will be true because we are using
+              <input
+                type="text"
+                id="username"
+                className={`input-${theme}`}
+                placeholder="github username"
+                autoComplete="off"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+              {/* in the button element the disabled based on our this.state.username. when this.state.username has no value diabled will be true because we are using
           ! on this.state.username !undefined will be true. when this.state.username has a value which means user enter a username or text in the input
           disabled will false because this.state.username will be truthy => !true will be false
           look at dark-btn and dark-btn:disabled css code. dark-btn is applied when user enter data to input
           dark-btn:disabled is applied when input has no data
           */}
-          <button
-            className="btn dark-btn"
-            type="submit"
-            disabled={!this.state.username}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
+              <button
+                className={`btn ${theme == "dark" ? "light-btn" : "dark-btn"}`}
+                type="submit"
+                disabled={!this.state.username}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        )}
+      </ThemeConsumer>
     );
   }
 }
@@ -115,24 +133,28 @@ PlayerInput.propTypes = {
 
 function PlayerPreview({ username, onReset, label }) {
   return (
-    <div className="column player">
-      <h2 className="player-label">{label}</h2>
-      <div className="row bg-light">
-        <div className="player-info">
-          <img
-            className="avatar-small"
-            src={`https://github.com/${username}.png?size=200`}
-            alt={`Avatar for ${username}`}
-          />
-          <a href={`https://github.com/${username}`} className="link">
-            {username}
-          </a>
+    <ThemeConsumer>
+      {({ theme, toggleTheme }) => (
+        <div className="column player">
+          <h2 className="player-label">{label}</h2>
+          <div className={`row bg-${theme}`}>
+            <div className="player-info">
+              <img
+                className="avatar-small"
+                src={`https://github.com/${username}.png?size=200`}
+                alt={`Avatar for ${username}`}
+              />
+              <a href={`https://github.com/${username}`} className="link">
+                {username}
+              </a>
+            </div>
+            <button className="btn-clear flex-center" onClick={onReset}>
+              <FaTimesCircle color="rgb(194,57,42)" size={26} />
+            </button>
+          </div>
         </div>
-        <button className="btn-clear flex-center" onClick={onReset}>
-          <FaTimesCircle color="rgb(194,57,42)" size={26} />
-        </button>
-      </div>
-    </div>
+      )}
+    </ThemeConsumer>
   );
 }
 
