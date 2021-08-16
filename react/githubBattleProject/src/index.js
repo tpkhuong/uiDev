bbb0caa162d09d8b3e43140d7d993ef2c691ad0c
Toan <tpkhuong@gmail.com>
2021-08-16 06:52:41 -0400

    dark mode toggle js and css

--- react/githubBattleProject/src/index.js
+++ react/githubBattleProject/src/index.js
@@ -3,14 +3,37 @@ import ReactDOM from "react-dom";
 import "../public/styles.css";
 import Popular from "./components/PopularNavbar/PopularNavbar";
 import Battle from "./components/Battle/Battle";
+import { ThemeProvider } from "../context/theme";
+import Nav from "../src/components/Nav/Nav";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // whenever we want the UI to update based on some value in our app updating we will stick that value in our state
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme == "light" ? "dark" : "light",
+        }));
+      },
+    };
+  }
   render() {
     return (
-      <div className="container">
-        {/* <Popular /> */}
-        <Battle />
-      </div>
+      // <ThemeProvider> will take in a prop called value. whatever we passed to value prop
+      //will be available to any component inside of our application that use the <ThemeConsumer>(the consumer)
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            {/* <Popular /> */}
+            <Battle />
+          </div>
+        </div>
+      </ThemeProvider>
     );
   }
 }
