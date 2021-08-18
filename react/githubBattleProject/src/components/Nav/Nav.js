7658a6531ea0ef3c5e4f3063124b82fcbc7d770d
Toan <tpkhuong@gmail.com>
2021-08-18 06:50:32 -0400

    navbar react router nav js

--- react/githubBattleProject/src/components/Nav/Nav.js
+++ react/githubBattleProject/src/components/Nav/Nav.js
@@ -1,11 +1,49 @@
 import React from "react";
 import { ThemeConsumer } from "../../../context/theme";
+import { NavLink } from "react-router-dom";
+
+var activeStyle = {
+  color: "rgb(187,46,31)",
+};
 
 function Nav() {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className="row space-between">
+          <ul className="row nav">
+            {/* we will use NavLink instead of Link to pass extra props */}
+            {/* <li>
+              <Link to="/" className="nav-link">
+                Popular
+              </Link>
+            </li>
+            <li>
+              <Link to="/battle" className="nav-link">
+                Battle
+              </Link>
+            </li> */}
+            {/* using exact so that when we click on the link it will apply the red color to that link */}
+            <li>
+              <NavLink
+                exact
+                to="/"
+                className="nav-link"
+                activeStyle={activeStyle}
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/battle"
+                className="nav-link"
+                activeStyle={activeStyle}
+              >
+                Battle
+              </NavLink>
+            </li>
+          </ul>
           <button
             style={{ fontSize: 30 }}
             className="btn-clear"
