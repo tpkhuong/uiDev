cb6ab6444ec28290a4be03cd8dcded6b96c28744
Toan <tpkhuong@gmail.com>
2021-08-25 08:07:19 -0400

    tracking hackernewsclone dir

---
+++ react/hackerNewsCloneFunProject/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../public/styles.css";
+import Navbar from "./components/Navbar/Navbar";
+// Component
+// State
+// lifecycle
+// UI
+class App extends React.Component {
+  render() {
+    return <Navbar></Navbar>;
+  }
+}
+
+ReactDOM.render(
+  // React element
+  <App />,
+  // where to render the element to
+  document.getElementById("app")
+);
---
+++ react/hackerNewsCloneFunProject/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  mode: "development",
+};
