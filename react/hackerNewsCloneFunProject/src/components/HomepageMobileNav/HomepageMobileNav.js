761e6fe6af3216be1ad3ea865deeeae4a29caccf
Toan <tpkhuong@gmail.com>
2021-09-01 06:39:50 -0400

    tracking HomepageMobileDev dir

---
+++ react/hackerNewsCloneFunProject/src/components/HomepageMobileNav/HomepageMobileNav.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class HomepageMobileNav extends React.Component {
+  render() {
+    return (
+      <ul role="menu" className="navlist">
+        <li role="none" className="navlist">
+          <a href="#" role="menuitem" className="navlink">
+            How it work
+          </a>
+        </li>
+        <li role="none" className="navlist">
+          <a href="#" role="menuitem" className="navlink">
+            Blog
+          </a>
+        </li>
+        <li role="none" className="navlist">
+          <a href="#" role="menuitem" className="navlink">
+            Account
+          </a>
+        </li>
+        <li role="menuitem" className="navlist">
+          <button role="button" className="navbar-btn">
+            View Plans
+          </button>
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default HomepageMobileNav;
