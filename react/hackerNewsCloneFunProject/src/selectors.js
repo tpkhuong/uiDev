aa4eb80f500381116cae853c354574cc85181786
Toan <tpkhuong@gmail.com>
2021-09-03 06:28:11 -0400

    our selector js file

---
+++ react/hackerNewsCloneFunProject/src/selectors.js
@@ -0,0 +1,11 @@
+export function ourSelectors() {
+  //open btn
+  var openBtn = document.querySelector(".open-btn");
+  //close btn
+  var closeBtn = document.querySelector(".close-btn");
+
+  return {
+    openBtn,
+    closeBtn,
+  };
+}
