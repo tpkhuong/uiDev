9e032b1daad7a591afdc093dd7abcec43cea30d7
Toan <tpkhuong@gmail.com>
2021-09-12 06:49:33 -0400

    plan cards solution looking good

--- react/frontendMentorChallengesReactVer/pricingComponentToggle/src/components/PlanCards/PlanCards.js
+++ react/frontendMentorChallengesReactVer/pricingComponentToggle/src/components/PlanCards/PlanCards.js
@@ -13,9 +13,39 @@ class PlanCards extends React.Component {
   componentDidMount() {}
 
   render() {
+    const { pricePlan } = this.props;
+    const planCard = pricePlan == "false" ? "annually" : "monthly";
     return (
       <article>
-        {this.props.pricePlan == "false" ? "Annually" : "Monthly"}
+        {objOfDataForCards[planCard].map((obj, index) => {
+          const randomNumber = Math.random();
+          return (
+            <div
+              key={randomNumber}
+              className={`plan-card ${index == 1 ? "hello" : "there"} ${
+                index == 1 ? "word" : "Let's GOOOOO!!!"
+              }`}
+            >
+              {/* name */}
+              <span>{obj.price}</span>
+              {/* price wrapper */}
+              <span>
+                {/* dollar sign */}
+                <span></span>
+                {/* price */}
+                <span></span>
+              </span>
+              {/* storage */}
+              <span></span>
+              {/* users */}
+              <span></span>
+              {/* upload */}
+              <span></span>
+              {/* button */}
+              <a href=""></a>
+            </div>
+          );
+        })}
       </article>
     );
   }
