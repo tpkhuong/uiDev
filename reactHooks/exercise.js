69ff4b2519607ee2de4813662259559888167f2c
Toan <tpkhuong@gmail.com>
2021-10-23 07:05:24 -0400

    working on show/hide exercise react

--- reactHooks/exercise.js
+++ reactHooks/exercise.js
@@ -310,3 +310,115 @@ class Todo extends React.Component {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Todo />, rootElement);
+
+/**
+ * Solution
+ * **/
+
+function generateId() {
+  return "_" + Math.random().toString(36).substr(2, 9);
+}
+
+function Todo() {
+  // merging new todo to our current array
+  const [todos, setTodos] = React.useState([]);
+  const [input, setInput] = React.useState("");
+
+  function handleSubmit() {
+    setTodos((todos) => {
+      console.log(todos);
+      //   was getting an array because we didnt return a new copy of the array with the obj
+      //initial value of setTodos is an empty array []
+      return todos.concat({
+        text: input,
+        id: generateId(),
+      });
+    });
+    console.log("handleSubmit");
+    console.log("input", input);
+    setInput("");
+  }
+
+  function deleteTodos(id) {
+    setTodos((todos) => {
+      console.log("deleteTodos");
+      console.log("todos", todos);
+      console.log("id", id);
+      return todos.filter(function removeItem(item, index) {
+        return item.id !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={input}
+        placeholder="Add todos"
+        onChange={(event) => {
+          // setInput will update input
+          return setInput(event.target.value);
+        }}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      {/* <ul>
+        {todos.map(({ text, id }) => (
+          <li key={id}>
+            <span>{text}</span>
+            <button onClick={() => removeTodo(id)}>X</button>
+          </li>
+        ))}
+      </ul> */}
+      <ul role="menu" className="navlist">
+        {todos.map(({ text, id }) => {
+          // return in map,filter, reduce
+          return (
+            <li key={id} role="none" className="navitem">
+              <span>{text}</span>
+              <button
+                onClick={() => {
+                  deleteTodos(id);
+                }}
+              >
+                X
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+/**
+ * show/hide exercise
+ * **/
+
+function App({ posts }) {
+  return <div>{JSON.stringify(posts, null, 2)}</div>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <App
+    posts={[
+      {
+        id: 0,
+        img: "https://res.cloudinary.com/uidotdev/image/upload/c_fit,co_white,fl_text_no_trim,l_text:fonts:proxima-nova-extrabold.ttf_80_center_line_spacing_-18:Code%20Splitting%20with%20React%252C%20React.lazy%252C%20and%20React%20Router,w_1100/fl_layer_apply,y_-30/bo_2px_solid_white,fl_text_no_trim,h_126,l_twitter_name:tylermcginnis,r_max,w_126/fl_layer_apply,g_south_west,x_385,y_72/co_white,fl_text_no_trim,l_text:fonts:proxima-nova-semibold.ttf_38:by/fl_layer_apply,g_south_west,x_552,y_136/co_white,fl_text_no_trim,l_text:fonts:proxima-nova-extrabold.ttf_40:Tyler%20McGinnis/fl_layer_apply,g_south_west,x_610,y_135/co_white,fl_text_no_trim,l_text:fonts:proxima-nova-semibold.ttf_31:4%20minute%20read/fl_layer_apply,g_south_west,x_554,y_93/c_fill,g_north,h_630,w_1200/og-social-bg.png",
+        text: "Code splitting has gained popularity recently for its ability to allow you to split your app into separate bundles your users can progressively load. In this post we’ll take a look at not only what code splitting is and how to do it, but also how to implement it with React Router.",
+      },
+      {
+        id: 1,
+        img: "https://res.cloudinary.com/uidotdev/image/upload/c_fit,co_white,fl_text_no_trim,l_text:fonts:proxima-nova-extrabold.ttf_80_center_line_spacing_-18:JavaScript%20Inheritance%20vs%20Composition,w_1100/fl_layer_apply,y_-30/bo_2px_solid_white,fl_text_no_trim,h_126,l_twitter_name:tylermcginnis,r_max,w_126/fl_layer_apply,g_south_west,x_385,y_72/co_white,fl_text_no_trim,l_text:fonts:proxima-nova-semibold.ttf_38:by/fl_layer_apply,g_south_west,x_552,y_136/co_white,fl_text_no_trim,l_text:fonts:proxima-nova-extrabold.ttf_40:Tyler%20McGinnis/fl_layer_apply,g_south_west,x_610,y_135/co_white,fl_text_no_trim,l_text:fonts:proxima-nova-semibold.ttf_31:3%20minute%20read/fl_layer_apply,g_south_west,x_554,y_93/c_fill,g_north,h_630,w_1200/og-social-bg.png",
+        text: "The problem with object-oriented languages is they’ve got all this implicit environment that they carry around with them. You wanted a banana but what you got was a gorilla holding the banana and the entire jungle. - Joe Armstrong.",
+      },
+      {
+        id: 2,
+        img: "https://res.cloudinary.com/uidotdev/image/upload/c_fit,co_white,fl_text_no_trim,l_text:fonts:proxima-nova-extrabold.ttf_80_center_line_spacing_-18:JavaScript%20Modules:%20From%20IIFEs%20to%20CommonJS%20to%20ES6%20Modules,w_1100/fl_layer_apply,y_-30/bo_2px_solid_white,fl_text_no_trim,h_126,l_twitter_name:tylermcginnis,r_max,w_126/fl_layer_apply,g_south_west,x_385,y_72/co_white,fl_text_no_trim,l_text:fonts:proxima-nova-semibold.ttf_38:by/fl_layer_apply,g_south_west,x_552,y_136/co_white,fl_text_no_trim,l_text:fonts:proxima-nova-extrabold.ttf_40:Tyler%20McGinnis/fl_layer_apply,g_south_west,x_610,y_135/co_white,fl_text_no_trim,l_text:fonts:proxima-nova-semibold.ttf_31:13%20minute%20read/fl_layer_apply,g_south_west,x_554,y_93/c_fill,g_north,h_630,w_1200/og-social-bg.png",
+        text: "I’ve taught JavaScript for a long time to a lot of people. Consistently the most commonly under-learned aspect of the language is the module system. There’s good reason for that. Modules in JavaScript have a strange and erratic history. In this post we’ll walk through that history and you’ll learn modules of the past to better understand how JavaScript modules work today.",
+      },
+    ]}
+  />,
+  rootElement
+);
