45c652085fe7f965f1be1f1efd87c8bf2a73a87f
Toan <tpkhuong@gmail.com>
2021-12-04 15:11:41 -0500

    tracking data element dir

---
+++ react/hackerNewsCloneFunProject/src/components/DataElement/DataElement.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../../../public/styles.css";
+
+function DataElement(props) {
+  return <span id="data-element" hidden></span>;
+}
+
+// class DataElement extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return <span hidden></span>;
+//   }
+// }
+
+export default DataElement;
